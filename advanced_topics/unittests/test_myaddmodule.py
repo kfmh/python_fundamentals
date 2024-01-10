3a252e6df464f155cae3861db708bea666f87bf5
undrwolf <74624168+undrwolf@users.noreply.github.com>
2024-01-10 18:31:27 +0100

    save

--- advanced_topics/unittests/test_myaddmodule.py
+++ advanced_topics/unittests/test_myaddmodule.py
@@ -3,6 +3,17 @@ import unittest
 from myaddmodule import add
 
 class TestAddFunction(unittest.TestCase):
+    """
+    Test suite for the add function in myaddmodule.
+
+    Methods
+    -------
+    test_add_positive_numbers():
+        Tests the add function with positive integer inputs.
+    
+    test_add_negative_numbers():
+        Tests the add function with negative integer inputs.
+    """
     
     def test_add_postiive_numbers(self):
         self.assertEqual(add(1, 2), 3)
